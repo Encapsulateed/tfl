b2d2fbc22f7a3d6ae366aa56981d33c1fd89ad05
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-01-05 00:19:55 +0300

    linter.

--- lab5/main.dart
+++ lab5/main.dart
@@ -1,11 +1,11 @@
 import './src/utils/grammar.dart';
-import './src/state_machine./FSM.dart';
-import 'src/lr0/base/LR0State.dart';
+//import './src/state_machine./FSM.dart';
+//import 'src/lr0/base/LR0State.dart';
 import 'src/lr0/base/LR0fms.dart';
 
 void main(List<String> arguments) {
   Grammar g = Grammar.fromFile('input.txt');
-  LR0State state = LR0State(g);
+  // LR0State state = LR0State(g);
   //print(state);
   LR0_FMS fms = LR0_FMS(g);
   //fms.DumpToDOT();
--- lab5/src/lr0/base/LR0Fms.dart
+++ lab5/src/lr0/base/LR0Fms.dart
@@ -9,7 +9,7 @@ class LR0_FMS extends FSM {
     super.startStates.add(State.named(
         state.states[grammar.rules.toList()[0]]!.toList()[0].toString()));
 
-    var prev = super.startStates.toList()[0];
+    //var prev = super.startStates.toList()[0];
 
     for (var rule in grammar.rules) {
       for (var lr0_situations in state.states[rule]!.toList()) {
