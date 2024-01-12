7ae972ba88af949857f8ecba50997541da88cc18
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-01-12 10:33:17 +0300

    remake lr0 parser with 2 stacks

--- lab5/main.dart
+++ lab5/main.dart
@@ -1,4 +1,5 @@
 import './src/utils/grammar.dart';
+import 'src/classes/GSSNode.dart';
 import 'src/classes/GSStack.dart';
 import 'src/lr0/LR0Fms.dart';
 import 'src/lr0/LR0Table.dart';
@@ -7,8 +8,19 @@ import 'src/state_machine/FSM.dart';
 // import 'src/lr0/base/LR0Fms.dart';
 
 void main(List<String> arguments) {
+  GSStack<String> statusStack = GSStackImpl<String>();
+  Map<String, GSSNode<String>> StatusNodes = {};
+
+  StatusNodes["0"] = statusStack.push("0");
+  StatusNodes["1"] = statusStack.push("1", StatusNodes["0"]);
+  StatusNodes["2"] = statusStack.push("2", StatusNodes["1"]);
+  StatusNodes["3"] = statusStack.push("3", StatusNodes["1"]);
+  StatusNodes["4"] = statusStack.push("4", StatusNodes["2"]);
+  StatusNodes["4"] = statusStack.push("4", StatusNodes["3"]);
+
+  // print(StatusNodes["2"]!.degPrev());
   var g = Grammar.fromFile('input.txt');
 
   LR0Parser p = LR0Parser(g);
-  print(p.ParseGss('(n+n)+(n)', 2));
+  print(p.Parse('(n+n)'));
 }
--- lab5/src/lr0/lr0Parser.dart
+++ lab5/src/lr0/lr0Parser.dart
@@ -1,3 +1,4 @@
+import '../classes/GSSNode.dart';
 import '../classes/GSStack.dart';
 import '../utils/Action.dart';
 import '../utils/grammar.dart';
@@ -19,12 +20,17 @@ class LR0Parser {
     _table.logToFile();
   }
 
+  Set<String> calculatePossibleParsingSets(String token_peek,
+      String status_peek, String word, Action selectedAction) {
+    return {};
+  }
+
   bool Parse(String word) {
     Stack<String> inputStack = Stack();
     Stack<String> tokenStack = Stack();
-    Stack<String> statusStack = Stack();
+    //Stack<String> statusStack = Stack();
 
-    statusStack.push('0');
+    tokenStack.push('0');
     inputStack.push('\$');
 
     for (int i = word.length - 1; i >= 0; i--) {
@@ -32,7 +38,9 @@ class LR0Parser {
     }
 
     while (true) {
-      int state_id = int.parse(statusStack.peek());
+      //    print(tokenStack.peek());
+      int state_id = int.parse(tokenStack.peek());
+
       List<Action> action = [];
       try {
         action = _table.lr0_table[state_id]![inputStack.peek()]!;
@@ -48,46 +56,116 @@ class LR0Parser {
         print('КОНФЛИКТ!');
         return false;
       }
+
       var a = action[0];
 
       if (a.actionTitle.startsWith('ACC')) {
         print('WORD ACCEPTED!');
         return true;
       } else if (a.actionTitle.startsWith('s')) {
-        statusStack.push(a.stateNumber!.toString());
         tokenStack.push(inputStack.peek());
+        tokenStack.push(a.stateNumber!.toString());
         inputStack.pop();
       } else if (a.actionTitle.startsWith('r')) {
         var rule = _grammar.rules.toList()[a.ruleNumber!];
 
         for (int i = rule.right.length - 1; i >= 0; i--) {
-          if (rule.right[i] != tokenStack.peek()) {
-            break;
-          }
           tokenStack.pop();
-          statusStack.pop();
+          tokenStack.pop();
         }
+
+        state_id = int.parse(tokenStack.peek());
         tokenStack.push(rule.left);
 
-        statusStack.push(_table
-            .lr0_table[int.parse(statusStack.peek())]![tokenStack.peek()]![0]
-            .toString());
+        tokenStack.push(_table.lr0_table[state_id]![rule.left]![0].toString());
       }
     }
   }
 
+  String getStrFromStack(Stack<String> inputStack, {bool reverse = true}) {
+    StringBuffer sb = StringBuffer();
+    List<String> tokens = inputStack.toList();
+
+    if (reverse) {
+      for (int i = tokens.length - 1; i >= 0; i--) {
+        sb.write('${tokens[i]} ');
+      }
+    } else {
+      for (int i = 0; i < tokens.length; i++) {
+        sb.write('${tokens[i]} ');
+      }
+    }
+
+    return sb.toString();
+  }
+
   bool ParseGss(String word, int n) {
-    GSStack<String> inputStack = GSStackImpl<String>();
+    return false;
+  }
+
+  /*  Stack<String> inputStack = Stack<String>();
     GSStack<String> tokenStack = GSStackImpl<String>();
     GSStack<String> statusStack = GSStackImpl<String>();
 
-    statusStack.push('0');
+    Map<String, GSSNode<String>> tokenNodes = {};
+    Map<String, GSSNode<String>> StatusNodes = {};
+
+    StatusNodes["0"] = statusStack.push("0");
+
     inputStack.push('\$');
 
     for (int i = word.length - 1; i >= 0; i--) {
       inputStack.push(word[i]);
     }
-    print(inputStack);
-    return false;
-  }
+
+    while (true) {
+      for (var key in StatusNodes.keys) {
+        var curr_node = StatusNodes[key];
+        // Берём верхнее значение со стека Состояний
+        int state_id = int.parse(StatusNodes["0"]);
+
+        List<Action> action = [];
+        try {
+          action = _table.lr0_table[state_id]![inputStack.peek()]!;
+          if (action.length == 0) {
+            throw Exception();
+          }
+        } catch (e) {
+          print('Ошибка');
+          return false;
+        }
+
+        if (action.length > 1) {
+          // Здесь надо ветвить стек
+          print('КОНФЛИКТ!');
+          //return false;
+        }
+
+        var a = action[0];
+
+        if (a.actionTitle.startsWith('ACC')) {
+          print('WORD ACCEPTED!');
+          return true;
+        } else if (a.actionTitle.startsWith('s')) {
+          statusStack.push(a.stateNumber!.toString());
+          tokenStack.push(inputStack.peek());
+          inputStack.pop();
+        } else if (a.actionTitle.startsWith('r')) {
+          var rule = _grammar.rules.toList()[a.ruleNumber!];
+
+          for (int i = rule.right.length - 1; i >= 0; i--) {
+            if (rule.right[i] != tokenStack.peek()) {
+              break;
+            }
+            tokenStack.pop();
+            statusStack.pop();
+          }
+          tokenStack.push(rule.left);
+
+          statusStack.push(_table
+              .lr0_table[int.parse(statusStack.peek())]![tokenStack.peek()]![0]
+              .toString());
+        }
+      }
+    }*/
 }
